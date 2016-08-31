4fd24b11ceb046aaae9930302d9ca30f7364ea37
Tim Carter <tim.carter@senseye.io>
2016-08-31 10:56:43 +0100

    tica: rudimentary implementation: iterate over words then each word

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -6,9 +6,15 @@ var ErrMissingWords = errors.New("a dictionary must contain at least one word")
 
 func CreateAlphabet(words []string) ([]rune, error) {
 	var err error
+	var alphabet []rune
 	if len(words) == 0 {
 		err = ErrMissingWords
 	}
-	// TODO replace with impl
-	return []rune{'B'}, err
+	// TODO replace with full impl
+	for _, word := range words {
+		for _, r := range word {
+			alphabet = append(alphabet, r)
+		}
+	}
+	return alphabet, err
 }
